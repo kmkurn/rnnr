e40101dd93a50779b4b242662637f6a5f1451029
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-09 16:56:17 +0700

    Refactor test_runner.py to use runner fixture

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,7 +1,7 @@
 from unittest.mock import Mock, call
 import copy
 
-from rnnr import Event, Runner
+from rnnr import Event
 
 
 # https://stackoverflow.com/questions/29516339/how-to-mock-calls-to-function-that-receives-mutable-object-as-parameter
@@ -10,32 +10,25 @@ class DeepcopyMock(Mock):
         return super()._mock_call(*copy.deepcopy(args), **copy.deepcopy(kwargs))
 
 
-def make_runner():
-    return Runner()
-
-
 class TestRun:
-    def test_ok(self):
+    def test_ok(self, runner):
         mock_fn = Mock()
         batches = range(10)
-        runner = make_runner()
         runner.run(mock_fn, batches)
         assert mock_fn.mock_calls == [call(b) for b in batches]
 
-    def test_more_than_one_epoch(self):
+    def test_more_than_one_epoch(self, runner):
         mock_fn = Mock()
         batches, max_epoch = range(10), 5
-        runner = make_runner()
         runner.run(mock_fn, batches, max_epoch=max_epoch)
         assert mock_fn.mock_calls == [call(b) for _ in range(max_epoch) for b in batches]
 
 
 class TestAppendHandler:
-    def test_epoch_started(self):
+    def test_epoch_started(self, runner):
         mock_handler = DeepcopyMock()
         batches, max_epoch = range(10), 5
 
-        runner = make_runner()
         runner.append_handler(Event.EPOCH_STARTED, mock_handler)
         runner.run(Mock(), batches, max_epoch=max_epoch)
 
@@ -44,11 +37,10 @@ class TestAppendHandler:
             for e in range(1, max_epoch + 1)
         ]
 
-    def test_batch_started(self):
+    def test_batch_started(self, runner):
         mock_handler = DeepcopyMock()
         batches, max_epoch = range(10), 5
 
-        runner = make_runner()
         runner.append_handler(Event.BATCH_STARTED, mock_handler)
         runner.run(Mock(), batches, max_epoch=max_epoch)
 
@@ -58,12 +50,11 @@ class TestAppendHandler:
             for b in batches
         ]
 
-    def test_batch_finished(self):
+    def test_batch_finished(self, runner):
         mock_handler = DeepcopyMock()
         mock_fn = Mock(wraps=lambda b: b ** 2)
         batches, max_epoch = range(10), 5
 
-        runner = make_runner()
         runner.append_handler(Event.BATCH_FINISHED, mock_handler)
         runner.run(mock_fn, batches, max_epoch=max_epoch)
 
@@ -79,11 +70,10 @@ class TestAppendHandler:
             for b in batches
         ]
 
-    def test_epoch_finished(self):
+    def test_epoch_finished(self, runner):
         mock_handler = DeepcopyMock()
         batches, max_epoch = range(10), 5
 
-        runner = make_runner()
         runner.append_handler(Event.EPOCH_FINISHED, mock_handler)
         runner.run(Mock(), batches, max_epoch=max_epoch)
 
@@ -93,7 +83,7 @@ class TestAppendHandler:
         ]
 
 
-def test_stop():
+def test_stop(runner):
     mock_bfhandler = Mock()
     mock_efhandler = Mock()
     batches = range(10)
@@ -102,7 +92,6 @@ def test_stop():
         if state['batch'] == 3:
             runner.stop()
 
-    runner = make_runner()
     runner.append_handler(Event.BATCH_STARTED, bshandler)
     runner.append_handler(Event.BATCH_FINISHED, mock_bfhandler)
     runner.append_handler(Event.EPOCH_FINISHED, mock_efhandler)
