5f1d3baf2b0b3abf9ab6993a18a826cdc6b334f3
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-10 07:57:52 +0700

    Refactor a bit

--- rnnr/attachments.py
+++ rnnr/attachments.py
@@ -34,18 +34,18 @@ class ProgressBar(Attachment):
         self._pbar: tqdm
 
     def attach_on(self, runner: Runner) -> None:
-        runner.append_handler(Event.EPOCH_STARTED, self._create_pbar)
-        runner.append_handler(Event.BATCH_FINISHED, self._update_pbar)
-        runner.append_handler(Event.EPOCH_FINISHED, self._close_pbar)
+        runner.append_handler(Event.EPOCH_STARTED, self._create)
+        runner.append_handler(Event.BATCH_FINISHED, self._update)
+        runner.append_handler(Event.EPOCH_FINISHED, self._close)
 
-    def _create_pbar(self, state: dict) -> None:
+    def _create(self, state: dict) -> None:
         self._pbar = self._tqdm_cls(state['batches'], **self._kwargs)
 
-    def _update_pbar(self, state: dict) -> None:
+    def _update(self, state: dict) -> None:
         self._pbar.set_postfix(**self._get_stats(state))
         self._pbar.update(self._get_size(state))
 
-    def _close_pbar(self, state: dict) -> None:
+    def _close(self, state: dict) -> None:
         self._pbar.close()
 
 
