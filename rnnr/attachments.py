033e345cb0492322e567b5eb2fc4f92225d63a4a
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-14 19:09:52 +0700

    Add docstring to ProgressBar class

--- rnnr/attachments.py
+++ rnnr/attachments.py
@@ -13,11 +13,36 @@ class Attachment(abc.ABC):
 
 
 class ProgressBar(Attachment):
+    """An attachment to display a progress bar.
+
+    The progress bar is implemented using `tqdm`_.
+
+    Example:
+
+        >>> from rnnr import Runner
+        >>> from rnnr.attachments import ProgressBar
+        >>> runner = Runner()
+        >>> ProgressBar().attach_on(runner)
+        >>> runner.run(lambda x: x, range(10), max_epoch=10)
+
+    Args:
+        size_fn: Function to get the size of a batch to update the progress bar with.
+            If not given, the default is to always return 1 as the size of a batch.
+        stats_fn: Function to get the statistics dictionary to be displayed along with the
+            progress bar. If given, it should accept a runner's state dictionary and return
+            another dictionary whose keys are the names of the statistics and the values are
+            the statistics values.
+        **kwargs: Keyword arguments to be passed to `tqdm`_ class.
+
+
+    .. _tqdm: https://github.com/tqdm/tqdm
+    """
+
     def __init__(
             self,
-            tqdm_cls: Optional[Type[tqdm]] = None,
             size_fn: Optional[Callable[[dict], int]] = None,
             stats_fn: Optional[Callable[[dict], dict]] = None,
+            tqdm_cls: Optional[Type[tqdm]] = None,
             **kwargs,
     ) -> None:
         if tqdm_cls is None:  # pragma: no cover
@@ -35,6 +60,11 @@ class ProgressBar(Attachment):
         self._pbar: tqdm
 
     def attach_on(self, runner: Runner) -> None:
+        """Attach this progress bar to the given runner.
+
+        Args:
+            runner: Runner to attach this progress bar to.
+        """
         runner.append_handler(Event.EPOCH_STARTED, self._create)
         runner.append_handler(Event.BATCH_FINISHED, self._update)
         runner.append_handler(Event.EPOCH_FINISHED, self._close)
