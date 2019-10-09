28f5b178237cdab6ea8719ff24d4db248cdbcaa6
Kemal Kurniawan <kemal@kkurniawan.com>
2019-10-09 19:24:27 +1100

    Remove unused imports

--- rnnr/attachments.py
+++ rnnr/attachments.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, Optional, Type
+from typing import Optional, Type
 import abc
 
 from tqdm import tqdm
