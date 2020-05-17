8aa5040c8b57656a30b11bb322360a999fa2fcfb
Kemal Kurniawan <kemal@kkurniawan.com>
2020-05-17 15:50:14 +1000

    Update pre-commit hook and flake config comment

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Format all staged Python files (that are NOT deleted) with yapf
+# Format all staged Python files (that are NOT deleted)
 PYTHON_FILES=$(git diff --cached --name-only --diff-filter=d | grep -E '\.py')
 if [ -n "$PYTHON_FILES" ]; then
     for file in $PYTHON_FILES; do
-        yapf -i "$file" && git add "$file"
+        black "$file" && git add "$file"
     done
 fi
